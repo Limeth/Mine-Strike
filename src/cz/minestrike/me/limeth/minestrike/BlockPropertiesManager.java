6ef98eec05ad7ffa90bf88067e24de0aa9020580
Jakub Hlusicka <jakub.hlusicka@email.cz>
2015-06-14 21:55:09 +0200

    Created BlockProperties and their manager. Not finished.

---
+++ src/cz/minestrike/me/limeth/minestrike/BlockPropertiesManager.java
@@ -0,0 +1,130 @@
+package cz.minestrike.me.limeth.minestrike;
+
+import com.google.common.collect.Maps;
+import cz.minestrike.me.limeth.minestrike.equipment.BlockProperties;
+import cz.minestrike.me.limeth.minestrike.equipment.EquipmentManager;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonElement;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonParser;
+import org.bukkit.material.MaterialData;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Limeth
+ */
+public class BlockPropertiesManager
+{
+	public static final String                             GROUP_BLOCK_NAMESPACE = "namespace";
+	public static final String                             GROUP_BLOCK_ID        = "id";
+	public static final String                             GROUP_BLOCK_DATA      = "data";
+	public static final String                             REGEX_BLOCK           = "^((?<" + GROUP_BLOCK_NAMESPACE + ">[a-zA-Z0-9_]+):)??(?<" + GROUP_BLOCK_ID + ">[a-zA-Z0-9_]+)(:(?<" + GROUP_BLOCK_DATA + ">[0-9]+))?$";
+	public static final Pattern                            PATTERN_BLOCK         = Pattern.compile(REGEX_BLOCK);
+	public static final File                               FILE                  = new File("plugins/MineStrike/blocks.json");
+	public static final Map<Material, BlockProperties>     materialMap           = Maps.newHashMap();
+	public static final Map<MaterialData, BlockProperties> materialDataMap       = Maps.newHashMap();
+
+	public static void load()
+	{
+		materialMap.clear();
+		materialDataMap.clear();
+
+		JsonElement rawRoot = prepareFile();
+
+		if(!rawRoot.isJsonObject())
+			return;
+
+		JsonObject root = rawRoot.getAsJsonObject();
+
+		for(Map.Entry<String, JsonElement> entry : root.entrySet())
+		{
+			try
+			{
+				String target = entry.getKey();
+				Matcher matcher = PATTERN_BLOCK.matcher(target);
+				String namespace = matcher.group(GROUP_BLOCK_NAMESPACE);
+				String id = matcher.group(GROUP_BLOCK_ID);
+				String rawData = matcher.group(GROUP_BLOCK_DATA);
+				Byte data = rawData != null ? Byte.parseByte(rawData) : null;
+				Material material = Material.matchMaterial(id);
+				JsonElement element = entry.getValue();
+				BlockProperties properties = BlockProperties.parse(element);
+
+				if(data == null)
+				{
+					materialMap.put(material, properties);
+				}
+				else
+				{
+					MaterialData materialData = new MaterialData(material, data);
+
+					materialDataMap.put(materialData, properties);
+				}
+			}
+			catch(NumberFormatException | BlockProperties.BlockPropertiesParseException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static JsonElement prepareFile()
+	{
+		try
+		{
+			return prepareFileUnsafe();
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static JsonElement prepareFileUnsafe() throws IOException
+	{
+		if(!FILE.isFile())
+		{
+			if(FILE.exists() && !FILE.delete())
+				throw new IOException("Couldn't delete the blocks.json file.");
+
+			if(!FILE.getParentFile().isDirectory() && !FILE.getParentFile().mkdirs())
+				throw new IOException("Couldn't create a directory for the equipmentCustomizations.json file.");
+
+			if(!FILE.createNewFile())
+				throw new IOException("Couldn't create a new equipmentCustomizations.json file.");
+
+			JsonObject root = new JsonObject();
+			JsonObject glass = new JsonObject();
+			JsonObject pot = new JsonObject();
+
+			glass.addProperty(BlockProperties.KEY_PENETRATION, 1);
+			glass.addProperty(BlockProperties.KEY_DURABILITY, 0);
+			pot.addProperty(BlockProperties.KEY_DURABILITY, 0);
+			root.add("thin_glass", glass);
+			root.add("flower_pot", pot);
+			root.addProperty("wood", 0.5);
+
+			FileWriter writer = new FileWriter(FILE);
+
+			EquipmentManager.GSON_PRETTY.toJson(root, writer);
+			writer.close();
+
+			return root;
+		}
+
+		FileReader reader = new FileReader(FILE);
+		JsonParser parser = new JsonParser();
+		JsonElement result = parser.parse(reader);
+
+		reader.close();
+
+		return result;
+	}
+}
---
+++ src/cz/minestrike/me/limeth/minestrike/equipment/BlockProperties.java
@@ -0,0 +1,96 @@
+package cz.minestrike.me.limeth.minestrike.equipment;
+
+import com.google.common.base.Preconditions;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonElement;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
+
+/**
+ * @author Limeth
+ */
+public class BlockProperties
+{
+	public static final BlockProperties DEFAULT = new BlockProperties(0, null);
+	public static final String KEY_PENETRATION = "penetration";
+	public static final String KEY_DURABILITY = "durability";
+	private final double penetration;
+	private final Double durability;
+
+	public BlockProperties(double penetration, Double durability)
+	{
+		Preconditions.checkArgument(penetration >= 0 && penetration <= 1, "The penetration must be in range <0; 1>.");
+		Preconditions.checkArgument(durability == null || durability >= 0, "The durability must be larger or equal to 0, or null.");
+
+		this.penetration = penetration;
+		this.durability = durability;
+	}
+
+	public static BlockProperties parse(JsonElement element) throws BlockPropertiesParseException
+	{
+		try
+		{
+			if(element.isJsonPrimitive())
+				return new BlockProperties(element.getAsDouble(), null);
+			else
+			{
+				JsonObject object = element.getAsJsonObject();
+				double penetration = object.has(KEY_PENETRATION) ? object.get(KEY_PENETRATION).getAsDouble() : 0;
+				Double durability = object.has(KEY_DURABILITY) ? object.get(KEY_DURABILITY).getAsDouble() : null;
+
+				return new BlockProperties(penetration, durability);
+			}
+		}
+		catch(UnsupportedOperationException e)
+		{
+			throw new BlockPropertiesParseException(e);
+		}
+	}
+
+	public double getPenetration()
+	{
+		return penetration;
+	}
+
+	public double getDurability()
+	{
+		Preconditions.checkArgument(isBreakable(), "The block is not breakable.");
+
+		return durability;
+	}
+
+	public boolean isPenetrable()
+	{
+		return penetration > 0;
+	}
+
+	public boolean isBreakable()
+	{
+		return durability != null;
+	}
+
+	public static class BlockPropertiesParseException extends Throwable
+	{
+		private BlockPropertiesParseException()
+		{
+		}
+
+		private BlockPropertiesParseException(String message)
+		{
+			super(message);
+		}
+
+		private BlockPropertiesParseException(String message, Throwable cause)
+		{
+			super(message, cause);
+		}
+
+		private BlockPropertiesParseException(Throwable cause)
+		{
+			super(cause);
+		}
+
+		private BlockPropertiesParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+		{
+			super(message, cause, enableSuppression, writableStackTrace);
+		}
+	}
+}
